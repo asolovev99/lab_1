ff0a4f5f98cd72e38c617e3ca3f5ae039b3a50d1
Andrew Solovev <solovevaa@mer.ci.nsu.ru>
2020-10-17 10:34:00 +0700

    Made all tasks

--- lab_1.cpp
+++ lab_1.cpp
@@ -3,11 +3,67 @@
 
 #include <iostream>
 
+int is_simple(int number);
+
+int get_power(int number, int power);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    for (int i = 1; i <= 100; i++)
+    {
+        if ((i % 3 == 0) || (i % 5 == 0))
+        {
+            if (i % 3 == 0)
+            {
+                printf("Fizz");
+            }
+            if (i % 5 == 0)
+            {
+                printf("Buzz");
+            }
+        }
+        else
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
 
+int is_simple(int number)
+{
+    if (number <= 1)
+    {
+        printf("Number less than 2. Can't be simple or composite number");
+
+        return -1;
+    }
+    else
+    {
+        for (int i = 2; i <= (int)sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+int get_power(int number, int power)
+{
+    int result = 1;
+    for (int i = 1; i <= power; i++)
+    {
+        result *= number;
+    }
+
+    return result;
+}
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
