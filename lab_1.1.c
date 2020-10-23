ea9171e94106980356e570b9eb4d786e9c5aaf0b
Andrew Solovev <solovevaa@mer.ci.nsu.ru>
2020-10-23 19:04:20 +0700

    Don't know what changed

---
+++ lab_1.1.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        if ((i % 3 == 0) || (i % 5 == 0))
+        {
+            if (i % 3 == 0)
+            {
+                printf("Fizz");
+            }
+            if (i % 5 == 0)
+            {
+                printf("Buzz");
+            }
+        }
+        else
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
